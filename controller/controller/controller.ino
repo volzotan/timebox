1d9a363578deca914b05fde5f788a29b5873bb51
Christopher Getschmann <christopher@volzo.de>
2016-08-01 02:46:41 +0200

    controller: photocell

--- controller/controller/controller.ino
+++ controller/controller/controller.ino
@@ -4,12 +4,15 @@
 #define PIN_DISPLAY_EN          9
 #define PIN_THERM_EN
 #define PIN_CAMERA_EN
+#define PIN_PHOTOCELL_EN       A3
+#define PIN_PHOTOCELL          A2
 #define PIN_POTENTIOMETER       6
 #define PIN_PUSHBUTTON          7
 
 // OPTIONS
 
-#define USE_DISPLAY
+#define USE_DISPLAY             1
+#define USE_PHOTOCELL           1
 
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
@@ -43,16 +46,25 @@ void setup() {
 
 
 void loop() {
-  Serial.println(digitalRead(PIN_PUSHBUTTON));
-//  delay(3000);
-//  digitalWrite(9, LOW);
-//    delay(3000);
-//      digitalWrite(9, HIGH);
-//    
+  //Serial.println(digitalRead(PIN_PUSHBUTTON));
+
+  #ifdef USE_PHOTOCELL
+    digitalWrite(PIN_PHOTOCELL_EN, HIGH);
+    delay(100);
+    analogRead(PIN_PHOTOCELL);
+    delay(10);
+    Serial.println(analogRead(PIN_PHOTOCELL));
+    digitalWrite(PIN_PHOTOCELL_EN, LOW);
+  #endif
+
+  delay(500);
 }
 
 void initButtons() {
   pinMode(PIN_DISPLAY_EN,     OUTPUT);  
+  pinMode(PIN_PHOTOCELL_EN,   OUTPUT);  
+  
+  pinMode(PIN_PHOTOCELL,      INPUT);  
   pinMode(PIN_POTENTIOMETER,  INPUT);  
   pinMode(PIN_PUSHBUTTON,     INPUT);    
 }
