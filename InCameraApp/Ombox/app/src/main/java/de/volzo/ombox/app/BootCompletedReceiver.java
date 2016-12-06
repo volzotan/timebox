341baefbad033009a546207cbf35c9f08a9801a5
Christopher Getschmann <christopher@volzo.de>
2016-12-06 22:00:22 +0100

    Ombox

---
+++ InCameraApp/Ombox/app/src/main/java/de/volzo/ombox/app/BootCompletedReceiver.java
@@ -0,0 +1,12 @@
+package de.volzo.ombox.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Logger.info("Boot completed");
+    }
+}
---
+++ InCameraApp/Ombox/app/src/main/java/de/volzo/ombox/app/ImageActivity.java
@@ -0,0 +1,35 @@
+package de.volzo.ombox.app;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import com.sony.scalar.provider.AvindexStore;
+
+import java.io.InputStream;
+
+public class ImageActivity extends BaseActivity {
+    private Bitmap image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image);
+
+        ScalingBitmapView imageView = (ScalingBitmapView) findViewById(R.id.imageView);
+        long id = getIntent().getLongExtra("id", 0);
+
+        Uri baseUri = AvindexStore.Images.Media.getContentUri(AvindexStore.Images.Media.EXTERNAL_DEFAULT_MEDIA_ID);
+        InputStream is = AvindexStore.Images.Media.getScreennailInputStream(getContentResolver(), baseUri, id);
+        image = BitmapFactory.decodeStream(is);
+
+        imageView.setImageBitmap(image);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        image.recycle();
+        image = null;
+    }
+}
