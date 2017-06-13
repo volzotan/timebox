8a2aa19f6761df0202aed05e8a83fedabed1096a
Christopher Getschmann <christopher@volzo.de>
2017-06-13 16:45:58 +0200

    caterina bootloader for Arduino Leonardo

---
+++ controller10/caterina_bootloader/.dep/Caterina.o.d
@@ -0,0 +1,266 @@
+Caterina.o: Caterina.c Caterina.h Descriptors.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/USB.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/../../Common/Common.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/../../Common/ArchitectureSpecific.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/../../Common/CompilerSpecific.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/../../Common/Architectures.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/../../Common/Attributes.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/../../Common/BoardTypes.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/../../Common/Endianness.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/USBMode.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/USBTask.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/../../../Common/Common.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/USBMode.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/USBController.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../USBMode.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../Events.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../USBMode.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../USBTask.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/USBInterrupt_AVR8.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/../../../../Common/Common.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBMode.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/../Events.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBController.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../Device.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../StdDescriptors.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../Events.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../Endpoint.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/Endpoint_AVR8.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBTask.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBInterrupt.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/Device_AVR8.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/../StdDescriptors.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/../Endpoint.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../Endpoint.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../DeviceStandardReq.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../StdRequestType.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../USBTask.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../USBController.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../EndpointStream.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/EndpointStream_AVR8.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/Events.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/StdRequestType.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/StdDescriptors.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/DeviceStandardReq.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/Events.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/StdDescriptors.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/ConfigDescriptor.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/HostStandardReq.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/USBController.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/USBInterrupt.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/Device.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/Endpoint.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/DeviceStandardReq.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Core/EndpointStream.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Audio.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/../Core/USBMode.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/Audio.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../../USB.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/Audio.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/../../Core/StdDescriptors.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/CDC.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/CDC.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/CDC.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/HID.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/HID.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/HID.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/HIDParser.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/../../../../Common/Common.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/HIDReportData.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/../Common/HID.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/MassStorage.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/MassStorage.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/MassStorage.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/MIDI.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/MIDI.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/MIDI.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/Audio.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Printer.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/RNDIS.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/RNDIS.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/RNDIS.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/CDC.h \
+ lufa-LUFA-111009/LUFA/Drivers/USB/Class/StillImage.h
+
+Caterina.h:
+
+Descriptors.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/USB.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/../../Common/Common.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/../../Common/ArchitectureSpecific.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/../../Common/CompilerSpecific.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/../../Common/Architectures.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/../../Common/Attributes.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/../../Common/BoardTypes.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/../../Common/Endianness.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/USBMode.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/USBTask.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/../../../Common/Common.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/USBMode.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/USBController.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../USBMode.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../Events.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../USBMode.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../USBTask.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/USBInterrupt_AVR8.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/../../../../Common/Common.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBMode.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/../Events.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBController.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../Device.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../StdDescriptors.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../Events.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../Endpoint.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/Endpoint_AVR8.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBTask.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBInterrupt.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/Device_AVR8.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/../StdDescriptors.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/../Endpoint.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../Endpoint.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../DeviceStandardReq.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../StdRequestType.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../USBTask.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../USBController.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../EndpointStream.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/AVR8/../AVR8/EndpointStream_AVR8.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/Events.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/StdRequestType.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/StdDescriptors.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/DeviceStandardReq.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/Events.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/StdDescriptors.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/ConfigDescriptor.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/HostStandardReq.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/USBController.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/USBInterrupt.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/Device.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/Endpoint.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/DeviceStandardReq.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Core/EndpointStream.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Audio.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/../Core/USBMode.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/Audio.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../../USB.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/Audio.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/../../Core/StdDescriptors.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/CDC.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/CDC.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/CDC.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/HID.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/HID.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/HID.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/HIDParser.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/../../../../Common/Common.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/HIDReportData.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/../Common/HID.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/MassStorage.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/MassStorage.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/MassStorage.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/MIDI.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/MIDI.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/MIDI.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/Audio.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Printer.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/RNDIS.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/RNDIS.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/RNDIS.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/Device/../Common/CDC.h:
+
+lufa-LUFA-111009/LUFA/Drivers/USB/Class/StillImage.h:
