1d408ff45733a917e9d0fad98e0da88ac8a29247
Christopher Getschmann <christopher@volzo.de>
2019-03-08 00:35:06 +0100

    zerobox menu for display progress

--- zerobox/displaytest.py
+++ zerobox/displaytest.py
@@ -8,6 +8,7 @@ import time
 import os
 from PIL import ImageFont, Image
 import PIL.ImageOps  
+import yaml
 
 import datetime
 import sys
@@ -59,26 +60,34 @@ FONT_CHARACTER_WIDTH = 3
 # font = ImageFont.truetype("ves-4x5.ttf", 5)
 # FONT_CHARACTER_WIDTH = 4
 
-STATE_LOGO          = 0
-STATE_MENU          = 1
-STATE_CONFIG_ITEM   = 2
-STATE_DIALOG        = 3
-STATE_RUNNING       = 4
-STATE_RUNNING_MENU  = 5
+STATE_INIT          = 0
+STATE_LOGO          = 1
+STATE_MENU          = 2
+STATE_CONFIG_ITEM   = 3
+STATE_DIALOG        = 4
+STATE_RUNNING       = 5
+STATE_RUNNING_MENU  = 6
 STATE_IDLE          = 100
 
-config = {
-    "interval": {
-        "type": "time",
-        "min": 1,
-        "max": 3600,
-        "value": 10
-    },
-    "secondexposure": {
-        "type": "boolean",
-        "value": False
-    }
-}
+
+# state
+
+state               = STATE_INIT
+isInvalid           = True
+
+menu_selected       = 0
+
+selectedConfigItem  = None
+configItemValue     = None
+configItemPos       = 0 # Pointer to change digits of a configItems value
+
+
+menu_fix = [
+    # "[short overview menu]",
+    # "settings",
+    "start",
+    "shutdown"
+]
 
 def checkAndRestartOnFileChange():
     if getmtime(__file__) != own_mtime:
@@ -92,7 +101,15 @@ def cleanup():
     device.cleanup()
 
 
-def _aperture_to_str(value):
+def invalidate():
+    isInvalid = True
+
+
+def validate():
+    pass
+
+
+def _apertureToStr(value):
     e = 0.1
 
     aperture_values = [ "1.0", "1.2", "1.4", "2", 
@@ -104,6 +121,40 @@ def _aperture_to_str(value):
             return "F{}".format(a)
 
     return "F{0:.1f}".format(value)
+
+
+def _configToList(c):
+    clist = []
+    for key, value in c.items():
+        value["name"] = key
+        clist.append(value)
+
+    return clist
+
+
+def _changeConfigItem(item, value, pos, op):
+    if item["type"] == "int":
+        if op > 0:
+            if not "max" in item or value + 1 <= item["max"]:
+                value += 1
+        else:
+            if not "min" in item or value - 1 >= item["min"]:
+                value -= 1
+
+    elif item["type"] == "boolean":
+        value = not value
+
+    elif item["type"] == "float":
+        if op > 0:
+            if not "max" in item or value + .01 <= item["max"]:
+                value += .01
+            if not "min" in item or value - .01 <= item["min"]:
+                value -= .01
+
+    elif item["type"] == "time":
+        pass
+
+    return value
 
 
 def getKeyEvents():
@@ -151,19 +202,27 @@ def getKeyEvents():
     return keys
 
 
+def rect(c, coords, invert=False):
+    fill = COLOR1
+    if invert:
+        fill = COLOR0
+
+    draw.rectangle(coords, outline=None, fill=fill)
+
+
 def text(c, coords, t, invert=False, rightalign=False):
+    s = str(t)
+
     fill = COLOR1
     if invert:
         fill = COLOR0
 
     if rightalign:
-        coords[0] -= len(t) * FONT_CHARACTER_WIDTH + len(t) - 1
+        coords[0] -= len(s) * FONT_CHARACTER_WIDTH + len(s) - 1
         if coords[0] < 0:
             coords[0] = 0
 
-    c.text(coords, t.upper(), font=font, fill=fill)
-
-    # TODO rightalign
+    c.text(coords, s.upper(), font=font, fill=fill)
 
 
 def draw_running(draw, config, data):
@@ -281,53 +340,205 @@ def draw_logo(draw, data):
     draw.rectangle([(56, 38), (56, 59)], outline=None, fill=COLOR1)
 
 
-data = {}
-data["cam_0"]                           = {}
-data["cam_1"]                           = {}
+def draw_menu(draw, menu, selected_index):
+    viewmenu = menu
+    viewindex = selected_index
 
-data["cam_0"]["active"]                 = True
-data["cam_0"]["shutter"]                = "1.0"
-data["cam_0"]["aperture"]               = 11
-data["cam_0"]["iso"]                    = 300
-data["cam_0"]["exposurecompensation"]   = 1
-
-data["cam_1"]["active"]                 = True
-data["cam_1"]["shutter"]                = "1/300"
-data["cam_1"]["aperture"]               = 5.6
-data["cam_1"]["iso"]                    = 300
-data["cam_1"]["exposurecompensation"]   = 1
-
-logo = Image.open("logo.png")
-logo = PIL.ImageOps.invert(logo)
-logo = logo.convert("1")
-data["logo"] = logo
-data["devicename"] = "undefined"
-now = datetime.datetime.now()
-data["version"] = now.strftime("%d.%m.%y")
-data["free_space"] = 0.45
-
-state = STATE_RUNNING
-
-while True:
-    with canvas(device) as draw:
-
-        k = getKeyEvents()
-        if len(k) > 0:
-            print(*k)
-
-        if state == STATE_LOGO:
-            draw_logo(draw, data)
-            state = STATE_MENU
-            time.sleep(1.0)
-        elif state == STATE_MENU:
-            # draw_dialog(draw, "abort capture?", ["no", "yes"])
-            state = STATE_RUNNING
-        elif state == STATE_RUNNING:
-            draw_running(draw, config, data)
-            #state = STATE_IDLE
+    if len(menu) > 8:
+        if selected_index > 8:
+            viewmenu = menu[selected_index-8:selected_index+1]
+            viewindex = 8
         else:
             pass
-            
-    time.sleep(0.1)
 
-    checkAndRestartOnFileChange()
+    for i in range(len(viewmenu)):
+        selected = False
+        if i == viewindex:
+            selected = True
+
+        if isinstance(viewmenu[i], dict): # it's a config item!
+            rect(draw, [(0, 1+7*i), (127, 7+7*i)], invert=not selected)
+            text(draw, [2, 2+7*i], viewmenu[i]["name"], invert=selected)
+            text(draw, [127, 2+7*i], viewmenu[i]["value"], invert=selected, rightalign=True)
+
+        elif viewmenu[i] == "-": # separator
+            rect(draw, [(0, 1+7*i), (127, 7+7*i)], invert=not selected)
+            rect(draw, [(0+1, 1+7*i+3), (127-1, 1+7*i+3)], invert=selected)
+
+        else: # command
+            rect(draw, [(0, 1+7*i), (127, 7+7*i)], invert=not selected)
+            text(draw, [2, 2+7*i], viewmenu[i], invert=selected)
+
+
+def draw_configItem(draw, item):
+
+    text(draw, [2, 2], item["name"])
+    rect(draw, [(0, 8), (100, 8)])
+    rect(draw, [(100, 0), (100, 64)])
+
+    text(draw, [127, 20], "BACK >", rightalign=True)
+    text(draw, [127, 50], "OK >", rightalign=True)
+
+    if item["type"] == "int":
+        text(draw, [50, 30], str(configItemValue))
+    elif item["type"] == "boolean":
+        pass
+    elif item["type"] == "float":
+        text(draw, [50, 30], "{0:.2f}".format(configItemValue))
+    elif item["type"] == "time":
+        pass
+    else:
+        print("unknown config item type: {}".format(item[type]))
+
+
+def draw_running_menu(draw, menu, selected):
+    for i in range(len(menu)):
+        if i == selected:
+            draw.rectangle([(0, 1+7*i), (127, 7+7*i)], outline=None, fill=COLOR1)
+            text(draw, [2, 2+7*i], menu[i], invert=True)
+        else:
+            text(draw, [2, 2+7*i], menu[i])
+
+
+if __name__ == "__main__":
+
+    config = None
+    with open("config.yaml", "r") as stream:
+        try:
+            config = yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    data = {}
+    data["cam_0"]                           = {}
+    data["cam_1"]                           = {}
+
+    data["cam_0"]["active"]                 = True
+    data["cam_0"]["shutter"]                = "1.0"
+    data["cam_0"]["aperture"]               = 11
+    data["cam_0"]["iso"]                    = 300
+    data["cam_0"]["exposurecompensation"]   = 1
+
+    data["cam_1"]["active"]                 = True
+    data["cam_1"]["shutter"]                = "1/300"
+    data["cam_1"]["aperture"]               = 5.6
+    data["cam_1"]["iso"]                    = 300
+    data["cam_1"]["exposurecompensation"]   = 1
+
+    logo = Image.open("logo.png")
+    logo = PIL.ImageOps.invert(logo)
+    logo = logo.convert("1")
+    data["logo"] = logo
+    data["devicename"] = "undefined"
+    now = datetime.datetime.now()
+    data["version"] = now.strftime("%d.%m.%y")
+    data["free_space"] = 0.45
+
+    while True:
+
+        # k = getKeyEvents()
+        # if len(k) > 0:
+        #     print(*k)
+
+
+        if state == STATE_INIT:
+            print("init")
+            state += 2 # TODO
+
+        elif state == STATE_LOGO:
+            if isInvalid:
+                with canvas(device) as draw:
+                    draw_logo(draw, data)
+            state = STATE_MENU
+            time.sleep(1.0)
+
+
+        elif state == STATE_MENU:
+            # draw_dialog(draw, "abort capture?", ["no", "yes"])
+
+            menu = menu_fix + ["-"] + _configToList(config)
+
+            if isInvalid:
+                with canvas(device) as draw:
+                    draw_menu(draw, menu, menu_selected)
+                validate()
+
+            k = getKeyEvents()
+
+            # if len(k) > 0:
+            #     print(*k)
+
+            if "u" in k:
+                menu_selected = (menu_selected - 1) % len(menu)
+                invalidate()
+            if "d" in k:
+                menu_selected = (menu_selected + 1) % len(menu)
+                invalidate()
+            if "c" in k:
+                selectedConfigItem = config[_configToList(config)[menu_selected-len(menu_fix)-1]["name"]]
+                configItemValue = selectedConfigItem["value"]
+                state = STATE_CONFIG_ITEM
+                invalidate()
+
+
+        elif state == STATE_CONFIG_ITEM:
+            if isInvalid:
+                with canvas(device) as draw:
+                    draw_configItem(draw, selectedConfigItem)
+                validate()
+
+            item = selectedConfigItem
+
+            k = getKeyEvents()
+            if "u" in k:
+                configItemValue = _changeConfigItem(selectedConfigItem, configItemValue, configItemPos, 1)
+                invalidate()
+            if "d" in k:
+                configItemValue = _changeConfigItem(selectedConfigItem, configItemValue, configItemPos, -1)
+                invalidate()
+            if "1" in k:
+                selectedConfigItem = None
+                configItemValue = None
+                state = STATE_MENU
+                invalidate()
+            if "3" in k:
+                selectedConfigItem["value"] = configItemValue
+                selectedConfigItem = None
+                configItemValue = None
+                state = STATE_MENU
+                invalidate()
+
+
+        elif state == STATE_RUNNING:
+            if invalidate:
+                with canvas(device) as draw:
+                    draw_running(draw, config, data)
+                validate()
+            #state = STATE_IDLE
+
+
+        elif state == STATE_RUNNING_MENU:
+
+            menu = ["back", "pause", "stop"]
+
+            if isInvalid:
+                with canvas(device) as draw:
+                    draw_running_menu(draw, menu, menu_selected)
+                validate()
+
+            k = getKeyEvents()
+            if len(k) > 0:
+                print(*k)
+            if "u" in k:
+                menu_selected = (menu_selected - 1) % len(menu)
+                invalidate()
+            if "d" in k:
+                menu_selected = (menu_selected + 1) % len(menu)
+                invalidate()
+
+        else:
+            pass
+                
+        #time.sleep(0.1)
+
+        checkAndRestartOnFileChange()
