2444debffb9d847d8f98e980265030cfea238be0
Christopher Getschmann <christopher@volzo.de>
2019-08-05 23:56:42 +0200

    buildroot changes

--- buildroot/pull_from_bollogg.sh
+++ buildroot/pull_from_bollogg.sh
@@ -1,2 +1,4 @@
 scp bollogg-local:/home/volzotan/buildroot_test/buildroot/configs/raspberrypi0w_defconfig .
+scp bollogg-local:/home/volzotan/buildroot_test/buildroot/configs/raspberrypi0_defconfig .
+scp bollogg-local:/home/volzotan/buildroot_test/buildroot/userfile .
 scp bollogg-local:/home/volzotan/buildroot_test/buildroot/.config .
