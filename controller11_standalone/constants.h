b435c3642900346d119be30e467074b66f9eceab
Christopher Getschmann <christopher@volzo.de>
2019-08-25 19:45:59 +0200

    controller11_standalone: trigger duration changes

--- controller11_standalone/constants.h
+++ controller11_standalone/constants.h
@@ -26,28 +26,30 @@
 
 #define PRE_TRIGGER_WAIT                  12
 
-#define TRIGGER_DURATION                   1
-// 1s for 3EV 3 image bracketing mode:
+#define TRIGGER_DURATION                   2
+// 2s for 3EV 3 image bracketing mode:
+//
 // if first and second image exposure time 
-// combined is less than 1s (= daytime), 
-// then 3 images are taken if longer only 
-// one or two
+// combined is less than 2s (= daytime), 
+// then 3 images are taken. 
+//
+// If time is longer then only one or two.
+//
+// 2s duration required to release even if
+// AF fails
 
 #define POST_TRIGGER_WAIT                 37             
 
 // ----------- MISC -----------
 
-#define VERSION                         11.0
+#define VERSION                         11.2
 
 #define VDBASEVOLTAGE                    5.1
 #define VDRESISTOR1                       10
 #define VDRESISTOR2                       10
 
 #define BATT_VD_RAW                        0
-#define BATT_CELL_1                        1
-#define BATT_CELL_2                        2 
 #define BATT_DIRECT                       -1 
-#define BATT_PERCENTAGE_CELL              -3
 #define BATT_PERCENTAGE_DIRECT            -4
 
 // ----------- ERROR CODES -----------
