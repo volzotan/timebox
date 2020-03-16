6f84e85e04b9a6c2b062cef43ff9fba05aa6d523
Christopher Getschmann <christopher@volzo.de>
2020-03-16 01:34:24 +0100

    bugfixes and improvements

--- zerobox/trashcam.py
+++ zerobox/trashcam.py
@@ -24,6 +24,7 @@ from PIL import Image
 SECOND_EXPOSURE_SHUTTER_SPEED   = 9
 SECOND_EXPOSURE_ISO             = 25
 THIRD_EXPOSURE_SHUTTER_SPEED    = SECOND_EXPOSURE_SHUTTER_SPEED*(2**7)
+FOURTH_EXPOSURE_SHUTTER_SPEED   = SECOND_EXPOSURE_SHUTTER_SPEED*(2**11)
 EXPOSURE_COMPENSATION           = 2 # 6 = +1 stop
 
 SHUTDOWN_ON_COMPLETE            = True 
@@ -37,6 +38,7 @@ WRITE_RAW                       = True
 OUTPUT_DIR_1                    = "captures_1"
 OUTPUT_DIR_2                    = "captures_2"
 OUTPUT_DIR_3                    = "captures_3"
+OUTPUT_DIR_4                    = "captures_4"
 OUTPUT_FILENAME                 = "cap"
 
 SERIAL_PORT                     = "/dev/ttyAMA0"
@@ -114,9 +116,9 @@ def read_exif_data(filename):
 
         log = logging.getLogger()
 
-        log.info("Exif exposure time  : {}".format(shutter_speed))
-        log.info("Exif aperture       : {}".format(aperture))
-        log.info("Exif ISO            : {}".format(iso))
+        log.info("Exif exposure time      : {}".format(shutter_speed))
+        log.info("Exif aperture           : {}".format(aperture))
+        log.info("Exif ISO                : {}".format(iso))
 
         ev = math.log(aperture / shutter_speed, 2) - math.log(iso/100, 2)
 
@@ -147,14 +149,14 @@ def print_exposure_settings(camera):
 
 def log_capture_info(camera, filename):
 
-    log.info("{:20s}: {}".format("filename", filename))
+    log.info("{:24s}: {}".format("filename", filename))
 
-    log.info("{:20s}: {}".format("shutter speed", camera.shutter_speed)) # microseconds
-    log.info("{:20s}: {}".format("exposure speed", camera.exposure_speed/(1000*1000)))
+    log.info("{:24s}: {}".format("shutter speed", camera.shutter_speed)) # microseconds
+    log.info("{:24s}: {:.10}".format("exposure speed", camera.exposure_speed/(1000*1000)))
 
-    log.info("{:20s}: {}".format("iso", camera.iso))
-    log.info("{:20s}: {:4.2f}".format("analog gain", float(camera.analog_gain)))
-    log.info("{:20s}: {:4.2f}".format("digital gain", float(camera.digital_gain)))
+    log.info("{:24s}: {}".format("iso", camera.iso))
+    log.info("{:24s}: {:4.2f}".format("analog gain", float(camera.analog_gain)))
+    log.info("{:24s}: {:4.2f}".format("digital gain", float(camera.digital_gain)))
 
 
 def calculate_brightness(filename):
@@ -185,6 +187,18 @@ def get_filename(extension): # returns(path, filename.ext)
     raise Exception("no filenames left!")
 
 
+def run_subprocess(cmd):
+
+    # combine STDERR and STDOUT to STDOUT
+    subp = subprocess.run(cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = subp.stdout.decode("utf-8")
+
+    if output[-1] == "\n":
+        output = output[:-1]
+
+    return output
+
+
 def global_except_hook(exctype, value, tb):
     
     log = logging.getLogger()
@@ -196,6 +210,7 @@ def global_except_hook(exctype, value, tb):
     subprocess.call(["sync"])
     sleep(1.0)
 
+    print("sync called. traceback:")
     traceback.print_tb(tb)
 
     exit()
@@ -229,7 +244,7 @@ def trigger():
     # give the 3A algorithms some time for warmup
     sleep(1)
 
-    log.debug("---- exposure 1 ----")
+    log.debug("------ exposure 1 ------")
 
     filename = get_filename(IMAGE_FORMAT)
     full_filename = os.path.join(*filename)
@@ -243,12 +258,12 @@ def trigger():
     first_exposure_ev = read_exif_data(full_filename)
     image_info.append([full_filename, first_exposure_ev])
 
-    log.info("brightness          : {:.2f} EV".format(first_exposure_ev))
+    log.info("brightness              : {:.2f} EV".format(first_exposure_ev))
     # if ND_FILTER is not None:
     #     log.info("brightness (incl ND): {:.2f} EV".format(first_exposure_ev+ND_FILTER))
     # print_exposure_settings(camera)
 
-    log.debug("---- exposure 2 ----")
+    log.debug("------ exposure 2 ------")
 
     # increase framerate, otherwise capture will block even on short exposures 
     # for several seconds (for some reason too fast rates (> 16fps) will result 
@@ -278,7 +293,7 @@ def trigger():
     # read_exif_data(full_filename_2)
     # print_exposure_settings(camera)
 
-    log.debug("---- exposure 3 ----")
+    log.debug("------ exposure 3 ------")
 
     camera.shutter_speed = THIRD_EXPOSURE_SHUTTER_SPEED
 
@@ -289,6 +304,18 @@ def trigger():
     log_capture_info(camera, full_filename_3)
 
     image_info.append([full_filename_3, None])
+
+    log.debug("------ exposure 4 ------")
+
+    camera.shutter_speed = FOURTH_EXPOSURE_SHUTTER_SPEED
+
+    sleep(0.1)
+
+    full_filename_4 = os.path.join(OUTPUT_DIR_4, filename[1])
+    camera.capture(full_filename_4, format=IMAGE_FORMAT)
+    log_capture_info(camera, full_filename_4)
+
+    image_info.append([full_filename_4, None])
 
     camera.close()
 
@@ -302,7 +329,7 @@ if __name__ == "__main__":
 
     # ---------------------------------------------------------------------------------------
 
-    log_filename = "trashcam.log"
+    log_filename = "log_trashcam.log"
 
     # create logger
     log = logging.getLogger() #"oneshot")
@@ -365,6 +392,8 @@ if __name__ == "__main__":
 
     log.info("--------------------------")
 
+    log.info("system status [start]: {}".format(run_subprocess("vcgencmd get_throttled")))
+
     try: 
         os.makedirs(OUTPUT_DIR_1)
         log.debug("created dir: {}".format(OUTPUT_DIR_1))
@@ -383,11 +412,18 @@ if __name__ == "__main__":
     except FileExistsError as e:
         pass
 
+    try: 
+        os.makedirs(OUTPUT_DIR_4)
+        log.debug("created dir: {}".format(OUTPUT_DIR_4))
+    except FileExistsError as e:
+        pass
+
     image_info = None
     
-    controller = TimeboxController.find_by_portname(SERIAL_PORT)
-    if controller is not None:
-        try:
+    try:
+        controller = TimeboxController.find_by_portname(SERIAL_PORT)
+        
+        if controller is not None:
 
             log.debug("controller found: {}".format(controller))
 
@@ -415,10 +451,10 @@ if __name__ == "__main__":
 
             d = datetime.fromtimestamp(secs)
             log.debug("setting system time to {}".format(d.strftime('%Y-%m-%d %H:%M:%S')))
-        except Exception as e:
-            log.error("setting system time failed: {}".format(e))
-    else:
-        log.warning("setting system time failed, no controller found")
+        else:
+            log.warning("setting system time failed, no controller found")
+    except Exception as e:
+        log.error("setting system time failed: {}".format(e))
 
     try:
 
@@ -447,108 +483,122 @@ if __name__ == "__main__":
         else:
             image_info = trigger()
 
+        log.info("--------------------------")
+
         diff = datetime.now().timestamp() - start
-        log.debug("total runtime: {:.3f} sec".format(diff))
+        log.debug("total runtime           : {:.3f} sec".format(diff))
 
     except Exception as e:
         log.error("error: [{}] {}".format(type(e), e))
 
     # ---------------------------------------------------------------------------------------
 
+    temp = None
+    try: 
+        temp_str = str(subprocess.check_output(["vcgencmd", "measure_temp"]))
+        temp = float(temp_str[temp_str.index("=")+1:temp_str.index("'")])
+    except Exception as e:
+        log.warn("reading pi temperature failed: {}".format(e))
+    log.info("temperature [pi]        : {:.2f}".format(temp))
+
+    # get EV of last (primary) image and reduce if brighter than threshold
+    # 
+    # option A: use EXIF data to compute EV
+    #  problem: if a ND1000 filter is used, only very bright scenes do increase
+    #           the shutter speed above minimum (and thus change the computed EV)
+    #
+    # option B: calculate brightness (or non-zero pixels) of capture_2 or _3
+    #  problem: requires 2-3s per image! (total time with option B from powerup
+    #           to request-for-shutdown is 37s)
+    #
+    # option C: get the brightest pixel in capture_2
+    #           if > than threshold, sun must be present
+    #  problem: works well for increase interval, but how to know when to reduce?
+    #           slow (seems to take about 8s??)
+
+    brightness_1 = None
+    brightness_2 = None
+
+    try:
+        if image_info is not None and len(image_info) > 0:
+
+            # option A:
+
+            # brightness = image_info[0][1]
+
+            # if ND_FILTER is not None:
+            #     brightness += ND_FILTER
+
+            # # take images slower
+
+            # if brightness <= REDUCE_INTERVAL_EV_THRESHOLD:
+            #     log.debug("request interval reduction (EV: {:.2f} < {})".format(
+            #         brightness, REDUCE_INTERVAL_EV_THRESHOLD))
+            #     controller.reduce_interval()
+
+            # # take images faster
+
+            # if brightness > INCREASE_INTERVAL_EV_THRESHOLD:
+            #     log.debug("request interval increase (EV: {:.2f} > {})".format(
+            #         brightness, INCREASE_INTERVAL_EV_THRESHOLD))
+            #     controller.increase_interval()
+
+            # option B:
+
+            # filename_capture_1 = image_info[0][0]
+            # brightness_1 = calculate_brightness(filename_capture_1)
+            # log.info("brightness of capture_1: {:7.5f}".format(brightness_1))
+
+            filename_capture_2 = image_info[1][0]
+            brightness_2 = calculate_brightness(filename_capture_2)
+            log.info("brightness of capture_2 : {:7.5f}".format(brightness_2))
+
+            # option C:
+
+            # filename_capture_2 = image_info[1][0]
+            # image = Image.open(filename_capture_2)
+            # min_value, max_value = image.getextrema()
+
+            # if max_value is not None and max_value >= 100:
+
+            #     # take images faster
+
+            #     log.debug("request interval increase (max value: {:.2f} > {})".format(
+            #         max_value, 100))
+            #     controller.increase_interval()
+
+            # pass
+
+    except Exception as e:
+        log.error("increasing/reducing interval failed: {}".format(e))
+
+    log.info("system status [end]: {}".format(run_subprocess("vcgencmd get_throttled")))
+
     if SHUTDOWN_ON_COMPLETE:
 
         if controller is not None:
             try:
 
-                log.info("battery: {}".format(controller.get_battery_status()))
-                log.info("temperature: {}".format(controller.get_temperature()))
+                log.info("battery                 : {}".format(controller.get_battery_status()))
+                # log.info("temperature [controller]: {}".format(controller.get_temperature()))
 
-                #
-                # get EV of last (primary) image and reduce if brighter than threshold
-                # 
-                # option A: use EXIF data to compute EV
-                #  problem: if a ND1000 filter is used, only very bright scenes do increase
-                #           the shutter speed above minimum (and thus change the computed EV)
-                #
-                # option B: calculate brightness (or non-zero pixels) of capture_2 or _3
-                #  problem: requires 2-3s per image! (total time with option B from powerup
-                #           to request-for-shutdown is 37s)
-                #
-                # option C: get the brightest pixel in capture_2
-                #           if > than threshold, sun must be present
-                #  problem: works well for increase interval, but how to know when to reduce?
-                #           slow (seems to take about 8s??)
+                # if brightness_1 < 0.05:
 
-                try:
-                    if image_info is not None and len(image_info) > 0:
+                #     # take images slower
 
-                        # option A:
+                #     log.debug("request interval reduction (brightness: {:.5f} < {})".format(
+                #         brightness_1, 0.05))
+                #     controller.reduce_interval()
 
-                        # brightness = image_info[0][1]
+                if brightness_2 is not None and brightness_2 >= 0.0001:
 
-                        # if ND_FILTER is not None:
-                        #     brightness += ND_FILTER
+                    # take images faster
 
-                        # # take images slower
+                    log.debug("request interval increase (brightness: {:.5f} > {})".format(
+                        brightness_2, 0.0001))
+                    controller.increase_interval()
 
-                        # if brightness <= REDUCE_INTERVAL_EV_THRESHOLD:
-                        #     log.debug("request interval reduction (EV: {:.2f} < {})".format(
-                        #         brightness, REDUCE_INTERVAL_EV_THRESHOLD))
-                        #     controller.reduce_interval()
-
-                        # # take images faster
-
-                        # if brightness > INCREASE_INTERVAL_EV_THRESHOLD:
-                        #     log.debug("request interval increase (EV: {:.2f} > {})".format(
-                        #         brightness, INCREASE_INTERVAL_EV_THRESHOLD))
-                        #     controller.increase_interval()
-
-                        # option B:
-
-                        # filename_capture_1 = image_info[0][0]
-                        # brightness_1 = calculate_brightness(filename_capture_1)
-                        # log.info("brightness of capture_1: {:7.5f}".format(brightness_1))
-
-                        # filename_capture_2 = image_info[1][0]
-                        # brightness_2 = calculate_brightness(filename_capture_2)
-                        # log.info("brightness of capture_2: {:7.5f}".format(brightness_2))
-
-                        # if brightness_1 < 0.05:
-
-                        #     # take images slower
-
-                        #     log.debug("request interval reduction (brightness: {:.5f} < {})".format(
-                        #         brightness_1, 0.05))
-                        #     controller.reduce_interval()
-
-                        if brightness_2 is not None and brightness_2 >= 0.0005:
-
-                            # take images faster
-
-                            log.debug("request interval increase (brightness: {:.5f} > {})".format(
-                                brightness_2, 0.0005))
-                            controller.increase_interval()
-
-                        # option C:
-
-                        # filename_capture_2 = image_info[1][0]
-                        # image = Image.open(filename_capture_2)
-                        # min_value, max_value = image.getextrema()
-
-                        # if max_value is not None and max_value >= 100:
-
-                        #     # take images faster
-
-                        #     log.debug("request interval increase (max value: {:.2f} > {})".format(
-                        #         max_value, 100))
-                        #     controller.increase_interval()
-
-                        # pass
-
-                except Exception as e:
-                    log.error("increasing/reducing interval failed: {}".format(e))
-
-                controller.shutdown(delay=12000)
+                controller.shutdown(delay=15000)
 
                 log.debug("shutdown command sent")
                 log.info("POWEROFF")
@@ -556,8 +606,8 @@ if __name__ == "__main__":
                 log.debug("logging shutdown")
                 logging.shutdown()
 
-                # subprocess.call(["sync"])
-                # sleep(0.5)
+                subprocess.call(["sync"])
+                sleep(0.5)
 
                 subprocess.call(["poweroff"])
                 exit()
