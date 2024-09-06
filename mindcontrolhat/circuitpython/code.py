6e77d50a8523c1d87ef9a129860ebb90e670d8cc
Christopher Getschmann <christopher@volzo.de>
2024-09-06 15:55:24 +0200

    mindcontrolhat: libraries and CAD files

--- mindcontrolhat/circuitpython/code.py
+++ mindcontrolhat/circuitpython/code.py
@@ -1,11 +1,13 @@
 import time
+import math
+
 import board
 import pwmio
 import busio
 from digitalio import DigitalInOut, Direction
-import math
 
 from adafruit_motor import servo
+
 import displayio
 import terminalio
 from adafruit_display_text import label
@@ -28,6 +30,7 @@ mode            = "?"
 num_cycles      = 0
 time_start      = None
 cycle_start     = None
+i2c             = None
 
 # ---
 
@@ -44,51 +47,49 @@ blocker_servo = servo.Servo(pwm)
 
 # DISPLAY
 
-displayio.release_displays()
-i2c = busio.I2C(board.GP5, board.GP4)
-display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
-display = adafruit_displayio_ssd1306.SSD1306(display_bus, 
-    width=DISPLAY_SIZE[0], height=DISPLAY_SIZE[1])
+try:
+    displayio.release_displays()
+    i2c = busio.I2C(board.GP5, board.GP4)
+    display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
+    display = adafruit_displayio_ssd1306.SSD1306(display_bus, rotation=180, width=DISPLAY_SIZE[0], height=DISPLAY_SIZE[1])
+except Exception as e:
+    print("> no display found")
 
-splash = displayio.Group()
-display.show(splash)
+if i2c is not None:
+    splash = displayio.Group()
+    display.show(splash)
 
-palette = displayio.Palette(1)
-palette[0] = 0x000000
+    palette = displayio.Palette(1)
+    palette[0] = 0x000000
 
-color_bitmap = displayio.Bitmap(DISPLAY_SIZE[0], DISPLAY_SIZE[1], 1)
-bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=palette, x=0, y=0)
-splash.append(bg_sprite)
+    color_bitmap = displayio.Bitmap(DISPLAY_SIZE[0], DISPLAY_SIZE[1], 1)
+    bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=palette, x=0, y=0)
+    splash.append(bg_sprite)
 
-splash.append(label.Label(
-    terminalio.FONT, text="CYCLES:", color=0xFFFFFF, x=25, y=4
-))
+    splash.append(label.Label(terminalio.FONT, text="CYCLES:", color=0xFFFFFF, x=25, y=4))
+    splash.append(label.Label(terminalio.FONT, text="ELAPSED:", color=0xFFFFFF, x=25, y=16))
 
-splash.append(label.Label(
-    terminalio.FONT, text="ELAPSED:", color=0xFFFFFF, x=25, y=16
-))
+    splash.append(Rect(19, 0, 1, 20, fill=0xFFFFFF))
 
-splash.append(Rect(19, 0, 1, 20, fill=0xFFFFFF))
+    num_cycles_label        = label.Label(terminalio.FONT, text="?", color=0xFFFFFF, x=76, y=4)
+    time_elapsed_label      = label.Label(terminalio.FONT, text="?", color=0xFFFFFF, x=76, y=16)
+    time_till_trigger_label = label.Label(terminalio.FONT, text="?", color=0xFFFFFF, x=DISPLAY_SIZE[0]-18, y=DISPLAY_SIZE[1]-4)
+    mode_label              = label.Label(terminalio.FONT, text="?", color=0xFFFFFF, x=3, y=10)
 
-num_cycles_label        = label.Label(terminalio.FONT, text="?", color=0xFFFFFF, x=76, y=4)
-time_elapsed_label      = label.Label(terminalio.FONT, text="?", color=0xFFFFFF, x=76, y=16)
-time_till_trigger_label = label.Label(terminalio.FONT, text="?", color=0xFFFFFF, x=DISPLAY_SIZE[0]-18, y=DISPLAY_SIZE[1]-4)
-mode_label              = label.Label(terminalio.FONT, text="?", color=0xFFFFFF, x=3, y=10)
+    splash.append(num_cycles_label)
+    splash.append(time_elapsed_label)
+    splash.append(time_till_trigger_label)
+    splash.append(mode_label)
 
-splash.append(num_cycles_label)
-splash.append(time_elapsed_label)
-splash.append(time_till_trigger_label)
-splash.append(mode_label)
+    r1 = Rect(0, DISPLAY_SIZE[1]-8, 34, 8, fill=0xFFFFFF)
+    r2 = Rect(36, DISPLAY_SIZE[1]-8, 34, 8, fill=0xFFFFFF)
+    r3 = Rect(72, DISPLAY_SIZE[1]-8, 34, 8, fill=0xFFFFFF)
+    r4 = Rect(1, DISPLAY_SIZE[1]-7, DISPLAY_SIZE[0]-24, 6, fill=0x000000) # must be last element added to group splash
 
-r1 = Rect(0, DISPLAY_SIZE[1]-8, 34, 8, fill=0xFFFFFF)
-r2 = Rect(36, DISPLAY_SIZE[1]-8, 34, 8, fill=0xFFFFFF)
-r3 = Rect(72, DISPLAY_SIZE[1]-8, 34, 8, fill=0xFFFFFF)
-r4 = Rect(1, DISPLAY_SIZE[1]-7, DISPLAY_SIZE[0]-24, 6, fill=0x000000) # must be last element added to group splash
-
-splash.append(r1)
-splash.append(r2)
-splash.append(r3)
-splash.append(r4)
+    splash.append(r1)
+    splash.append(r2)
+    splash.append(r3)
+    splash.append(r4)
 
 print("> init")
 
@@ -104,7 +105,7 @@ def trigger():
     trigger_focus.value = False
     sleep(1.0)
 
-    print("trigger")
+    print("{:5.2f} | trigger {}".format(time.monotonic()-time_start, mode))
 
 
 def _time_string(t):
@@ -115,13 +116,15 @@ def _time_string(t):
     return "{:02}:{:02}:{:02}".format(h, m, s)
 
 
-def display():
-
-    time_till_trigger = int(cycle_start + INTERVAL_M*3 - time.monotonic())
+def update_display():
 
     num_cycles_label.text = str(num_cycles)
+
+    time_till_trigger = int(cycle_start + INTERVAL_M*3 - time.monotonic())
     time_till_trigger_label.text = "{}".format(time_till_trigger)
+
     mode_label.text = mode
+
     time_elapsed_label.text = _time_string((time.monotonic() - time_start))
 
     progress = int((DISPLAY_SIZE[0]-24) * (time.monotonic()-cycle_start)/(INTERVAL_M*3))
@@ -135,14 +138,15 @@ def sleep_till(t):
         diff = (t-time.monotonic())
 
         if diff < 0:
-            print("warning: negative sleep time!")
+            # print("warning: negative sleep time!")
             return
         if diff < SLEEP_DURATION:
             time.sleep(diff)
             return
         else:
             time.sleep(SLEEP_DURATION)
-            display()
+            if i2c is not None:
+                update_display()
 
 
 def sleep(s):
@@ -154,28 +158,29 @@ cycle_start = time_start
 
 while True:
 
-    cycle_start = time.monotonic()
     num_cycles += 1
 
-    blocker_servo.angle = SERVO_ANGLE_M  # M1
+    blocker_servo.angle = SERVO_ANGLE_M     # M1
     mode = "M1"
-    sleep(2)                             #   2
-    trigger()                            #   5  -- 2
-    sleep(2)                             #   7
+    sleep(2)                                #   2
+    trigger()                               #   5  -- 2
+    sleep(2)                                #   7
 
-    blocker_servo.angle = SERVO_ANGLE_A  # A1
+    blocker_servo.angle = SERVO_ANGLE_A     # A1
     mode = "A1"
-    sleep(2)                             #   9
-    trigger()                            #  12  -- 9
-    sleep_till(cycle_start+INTERVAL_M)   #  45       
+    sleep(2)                                #   9
+    trigger()                               #  12  -- 9
+    sleep_till(cycle_start+INTERVAL_M)      #  45       
 
     blocker_servo.angle = SERVO_ANGLE_M
     mode = "M2"
     sleep(2)          
-    trigger()                            # M2   -- 2 + 45
+    trigger()                               # M2   -- 2 + 45
     sleep_till(cycle_start+INTERVAL_M*2)             
 
     mode = "M3"
     sleep(2)
-    trigger()                            # M3   -- 2 + 45 + 45
-    sleep_till(cycle_start+INTERVAL_M*3) # 135
+    trigger()                               # M3   -- 2 + 45 + 45
+    sleep_till(cycle_start+INTERVAL_M*3)    # 135
+
+    cycle_start += INTERVAL_M*3
---
+++ mindcontrolhat/readme.md
@@ -0,0 +1,7 @@
+![Sony a6000 camera with a MindControlHat](media/header.png)
+
+This is the code and CAD files for the MindControlHat, a small 3d-printed gadget for Sony a6000 cameras to switch modes (P, A, S, M, ...) and trigger the shutter in intervals. This allows to capture image sequences for ultra-long exposure photography and [digital solargraphies](https://volzo.de/posts/solargraphy2/).
+
+Fusion360/Step CAD files can be found in /CAD
+
+EasyEDA files (including BOM) for the PCB can be found at the EasyEDA [project page](https://oshwlab.com/volzo/mindcontrolhat_copy_copy).
